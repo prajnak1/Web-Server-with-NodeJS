eda17f347984a699b237519814ec33f90d8c1abc
prajnak1 <126357555+prajnak1@users.noreply.github.com>
2024-06-04 16:28:32 -0400

    Add files via upload

---
+++ routes/root.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+module.exports = router;
+router.get('^/$|index(.html)?',(req,res)=>{
+    //res.sendFile('./views/index.html',{root:__dirname });
+    res.sendFile(path.join(__dirname,'..','views','index.html'));
+});
+router.get('/new-page.html',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..','views','new-page.html'));
+});
+router.get('/old-page.html',(req,res)=>{
+    res.redirect(301,'/new-page.html');//302 by default
+});
